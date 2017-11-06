e4d48e6759ed2cf95671a27c4d2dab7ef52bf033
Katie Ude <katie.ude@gmail.com>
2017-11-06 11:25:34 -0600

    handling ajax requests with react, refactoring fucntional components to class components, handling null props, and throttling search terms

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import VideoListItem from './video_list_item'
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video} />
+    return (
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        key={video.etag}
+        video={video} />
+    )
   })
 
   return (
